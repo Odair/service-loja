01acdf3884c4f5c8191ae590ff9f6f2a287b8c9f
odair <odair.santiago@prodaptech.com.br>
2020-05-02 15:29:58 -0300

    services

--- src/services/fornecedorService.js
+++ src/services/fornecedorService.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
 const buscarFornecedorPorEstado = async (estado) => {
-    return await axios.get(`http://192.168.99.100:31735/fornecedor/${estado}`);
+    return await axios.get(`http://fornecedor/${estado}`);
 }
 
 const realizaPedido = async (itens) => {
-    return await axios.post(`http://192.168.99.100:31735/pedido`, itens);
+    return await axios.post(`http://pedido`, itens);
 }
 
 module.exports = { buscarFornecedorPorEstado, realizaPedido }
